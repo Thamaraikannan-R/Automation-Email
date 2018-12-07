9ffba5d4b7ccb61ffb2fec599cd9076f4aab5897
Thamaraikannan R <44894488+Thamaraikannan-R@users.noreply.github.com>
2018-12-07 14:48:00 +0530

    Update Auto Email.py

--- Auto Email.py
+++ Auto Email.py
@@ -3,8 +3,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 #return redirect(url_for('success',name = user))
-me = "rtkcse2@gmail.com"
-you = "rtkcse@gmail.com"
+me = "From email ID"
+you = "TO email ID"
 
        # Create message container - the correct MIME type is multipart/alternative.
 msg = MIMEMultipart('alternative')
@@ -19,6 +19,6 @@ msg.attach(part1)
 mail = smtplib.SMTP('smtp.gmail.com', 587)
 mail.ehlo()
 mail.starttls()
-mail.login('rtkcse2@gmail.com', 'kannankannan')
+mail.login('from email id', 'password')
 mail.sendmail(me, you, msg.as_string())
 print("Sucessfully email Sent")
